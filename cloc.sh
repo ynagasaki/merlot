a3b13d096e88c1266851a9c5e12e10e89ee82cf4
Yoshiyuki Nagasaki <yoshiyuk@frontdupe-lm.(none)>
2013-10-20 13:39:29 -0700

    updated cloc

--- cloc.sh
+++ cloc.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-find source -name *.hx | xargs wc -l | grep total | sed 's/ //g' | sed 's/total/ lines of code./g'
+
+if [[ $# > 0 && $1 = '-v' ]]; then
+	find source -name *.hx | xargs wc -l;
+elif [[ $# == 0 ]]; then
+	find source -name *.hx | xargs wc -l | grep total | sed 's/ //g' | sed 's/total/ lines of code./g';
+else
+	echo "usage: cloc.sh [-v]";
+fi
+
