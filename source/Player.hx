cfa81114c322f4e18ba94d9433866fa1fab1319a
Yoshiyuki Nagasaki <yoshiyuk@frontdupe-lm.(none)>
2013-12-26 15:30:53 -0800

    (#19) made a bigger pino-run sprite sheet; made appropriate changes in Player class

--- source/Player.hx
+++ source/Player.hx
@@ -5,32 +5,37 @@ import org.flixel.FlxObject;
 import org.flixel.FlxSprite;
 
 class Player extends Character {
-	public static inline var WIDTH : Float = 14;
+	public static inline var WIDTH : Float = 40;//14;
 	public static inline var WIDTH_HALF : Float = WIDTH / 2;
-	public static inline var HEIGHT : Float = 29;
+	public static inline var HEIGHT : Float = 75;//29;
 
 	public function new(X:Float, Y:Float) : Void {
 		super(X, Y);
-		loadGraphic("assets/pino-run.png", true, true, 40, 35);
-		maxVelocity.x = 100;			//walking speed
+		loadGraphic("assets/pino-run.png", true, true, 100, 94);//40, 35);
+		maxVelocity.x = 160;			//walking speed
 		drag.x = maxVelocity.x*4;		//deceleration (sliding to a stop)
 		
 		//tweak the bounding box for better feel
 		width = WIDTH;
 		height = HEIGHT;
 
-		frames = 5;
-		frameWidth = 40;
-		frameHeight = 35;
+		frames = 10;//5;
+		frameWidth = 100;//40;
+		frameHeight = 94;//35;
 
 		offset.x = frameWidth / 2 - WIDTH_HALF - 1;
 		offset.y = frameHeight - HEIGHT; // as long as frameheight > height
 
-		addAnimation("idle",[0],0,false);
+		/*addAnimation("idle",[0],0,false);
 		addAnimation("walk",[1,2,3,4],10,true);
 		addAnimation("walk_back",[1,2,3,4],10,true);
 		addAnimation("flail",[0],18,false);
-		addAnimation("jump",[1],0,false);
+		addAnimation("jump",[1],0,false);*/
+		addAnimation("idle",[0],0,false);
+		addAnimation("walk",[0,1,2,3,4,5,6,7,8,9],14,true);
+		addAnimation("walk_back",[0,1,2,3,4,5,6,7,8,9],14,true);
+		addAnimation("flail",[0],18,false);
+		addAnimation("jump",[0],0,false);
 	}
 
 	public function jump() : Void {
